e181557c637f992ad165807dafdbf75b30300a9a
Char B <charburton18@gmail.com>
2024-08-15 13:25:43 -0700

    Restate/Examples done for reverseWords.js

--- CodeWars/reverseWords.js
+++ CodeWars/reverseWords.js
@@ -5,5 +5,31 @@
 // "double  spaces"      ==> "elbuod  secaps"
 
 function reverseWords(str) {
-  
+
 }
+
+/*
+Restate:
+Is each letter flipped over it's y-axis? No
+Is each word in the same order? Or reverse order? ********YES Same order of words.*********
+Capitalization retained? Yes
+Punctuation and spaces retained? Yes, if it's part of a word it gets treated like a letter "hi!" -> "!ih"
+What data type goes in? Out? A string goes in "hi there" -> "ih ereht"
+What does the function output if it receives a non-string datatype? Number? Object? Null? Undefined?
+
+Examples:
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+1                     ==> "Sorry, please use quotes around your phrase."
+"!? !?"               ==> "?! ?!"
+
+Approach/Pseudocode:
+
+Code:
+
+Test:
+
+Optimize:
+
+
+*/
