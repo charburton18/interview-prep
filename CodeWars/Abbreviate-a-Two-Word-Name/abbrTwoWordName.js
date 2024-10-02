2e526feb7a37121a4a4ba34706e931f822b78a3e
Char B <charburton18@gmail.com>
2024-10-02 14:09:27 -0700

    began abbrTwoWordName.js

---
+++ CodeWars/Abbreviate-a-Two-Word-Name/abbrTwoWordName.js
@@ -0,0 +1,35 @@
+function abbrevName(name) {
+
+}
+
+/*
+Restate:
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+The output should be two capital letters with a dot separating them.
+It should look like this (see Examples)
+
+My Questions:
+What is the input? A string with only one space in it (between letters)
+What is the output? A string consisting of a capital letter, a dot, another capital letter
+What should the function do?
+- Define the first letter as firstInitial
+- Define the letter after the space as lastInitial
+- use toUppercase() on the first letter and the first letter after the space
+- return the two upper-cased letter with a '.' in between them
+
+Examples:
+Sam Harris => S.H
+patrick feeney => P.F
+a b => A.B
+
+
+Approach:
+
+Code:
+
+Test:
+
+Optimize:
+
+
+*/
