1e1d34b293555792117170eaf9fda43fe297f23f
Char B <charburton18@gmail.com>
2024-10-02 14:10:53 -0700

    added url

--- CodeWars/Abbreviate-a-Two-Word-Name/abbrTwoWordName.js
+++ CodeWars/Abbreviate-a-Two-Word-Name/abbrTwoWordName.js
@@ -3,6 +3,9 @@ function abbrevName(name) {
 }
 
 /*
+
+https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/javascript
+
 Restate:
 Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 The output should be two capital letters with a dot separating them.
