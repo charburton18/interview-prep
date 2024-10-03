2212ebc82d1f2abadd4a7f6b03230025a8f24898
Char B <charburton18@gmail.com>
2024-10-03 16:13:17 -0700

    started coding

--- CodeWars/Abbreviate-a-Two-Word-Name/abbrTwoWordName.js
+++ CodeWars/Abbreviate-a-Two-Word-Name/abbrTwoWordName.js
@@ -1,7 +1,3 @@
-function abbrevName(name) {
-
-}
-
 /*
 
 https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/javascript
@@ -14,11 +10,7 @@ It should look like this (see Examples)
 My Questions:
 What is the input? A string with only one space in it (between letters)
 What is the output? A string consisting of a capital letter, a dot, another capital letter
-What should the function do?
-- Define the first letter as firstInitial
-- Define the letter after the space as lastInitial
-- use toUppercase() on the first letter and the first letter after the space
-- return the two upper-cased letter with a '.' in between them
+What should the function do? (see Approach)
 
 Examples:
 Sam Harris => S.H
@@ -27,8 +19,18 @@ a b => A.B
 
 
 Approach:
+- Define the first letter as firstInitial
+- Define the letter after the space as lastInitial
+- use toUppercase() on the first letter and the first letter after the space
+- return the two upper-cased letters with a '.' in between them
 
 Code:
+*/
+function abbrevName(name) {
+
+}
+
+/*
 
 Test:
 
