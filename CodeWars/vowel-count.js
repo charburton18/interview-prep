8a9a0829298ddfb3c2f248849afa8a09852067d4
Char B <charburton18@gmail.com>
2024-11-05 15:16:23 -0700

    began vowel-count.js

---
+++ CodeWars/vowel-count.js
@@ -0,0 +1,25 @@
+/*
+https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+
+Restate:
+- input: string with lowercase letters and/or spaces
+- output: a number equal to the number of vowels in the string (a, e, i, o, u)
+
+
+Examples:
+
+Approach:
+
+Code:
+
+Test:
+
+Optimize:
+*/
